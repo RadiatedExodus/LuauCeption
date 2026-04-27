64a1d0a0a872b26674c03bc9a8096a1ff1c454cb
RadiatedExodus <me@repn.my.id>
2026-04-28 01:33:21 +0700

    repo: nuke everything

--- src/LuauCeptionFlags.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-#include <emscripten.h>
-#define LC_API extern "C" EMSCRIPTEN_KEEPALIVE
-
-LC_API bool set_flag(std::string name, bool state);
-LC_API void set_all_flags(bool state);
-LC_API void reset_flags();
